d94cb09d5621edabe4fdad57f99a9ce39dad68dd
Fieruza <fieruzt@gmail.com>
2020-03-03 16:44:47 -0500

    update Login.java

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -2,7 +2,7 @@ package Test;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
 //A User Story can have between 2-70 test cases- it DEPENDS
